1de46e7bb6aeaa46d1e6f2d3c71dfac25d9cd1b6
Jérémie Picard <jeremie.picard@zenika.com>
2023-08-30 11:28:23 +0200

    feat(diffusionWarning): display diffusion warning in details html tag

---
+++ src/components/icons/Contribute.jsx
@@ -0,0 +1,10 @@
+export default function IconContribute({width, height}) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" width={width} height={height} viewBox="0 0 24 24"
+             strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3h-6m12 0a9 9 0 11-18 0 9 9 0 0118 0"/>
+        </svg>
+    );
+}
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,15 @@ import Talks from "@/components/elements/Talks/Talks";
 import Link from "next/link";
 import IconLogout from "@/components/icons/Logout";
 import IconPublic from "@/components/icons/Public";
+import useLocalStorageOnce from "@/hooks/useLocalStorageOnce";
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 const nunito = Nunito({ subsets: ["latin"] });
 
 export default function Index() {
   const { user, error, isLoading } = useUser();
+
+  const [diffusionMessageRead, setDiffusionMessageAsRead] = useLocalStorageOnce(false, "diffusionMessageRead");
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -43,50 +46,55 @@ export default function Index() {
           </Link>
         </header>
 
-        <p className="jumbo text-center py-2 border-b">
-          Librairie de formation interne Zenika
-        </p>
-        <section className="description text-center text-sm py-3 px-2">
-          <p>
-            Listing de support d&apos;upskilling pour les consultants Zenika.
-            Ces médias sont des productions internes, et ne doivent pas être
-            diffusées hors de Zenika sauf mention contraire explicite{" "}
-            <IconPublic />. Chaque média est daté et le contexte de présentation
-            est précisé ici.
-          </p>
-          <p>
-            Sont éligibles ici les vidéos avec contenu de formation, avancé ou
-            non, permettant aux consultants internes de découvrir ou
-            d&apos;approfondir un sujet. Les contenus commerciaux ne sont pas la
-            cible de cette librairie.
-          </p>
-          <p>
-            Pourquoi un Site ? Parce qu&apos;on est trop mignon, et que git
-            n&apos;est pas adapté à toutes les populations 🤔🧌 Si vous avez
-            mieux comme outils, n&apos;hésitez pas 😄
-          </p>
-          <p>
-            La documentation technique et la documentation sur l&apos;ajout d&apos;un talk est
-            disponible&nbsp;
-            <a
-              href="https://docs.google.com/document/d/1FMLZWCNmCYxix0cRr0Ars0UnaZvoQQpRdRNnXGIzKOo/edit?usp=sharing"
-              target="_blank"
-              title="Lien vers la documentation de TheZaurus"
-              rel="noopener"
-            >
-              ici
-            </a>
-            . Vous pouvez également accéder directement au formulaire d&apos;ajout en cliquant sur&nbsp;
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSfF99AC8fEKcXXF6z-gZMeCCtSGyUvQYxjO03Z1SkP2k3yNrg/viewform"
-              target="_blank"
-              title="Lien vers le formulaire d'ajout"
-              rel="noopener"
-            >
-              ce lien
-            </a>.
-          </p>
+        <section className="description text-center text-sm mx-auto my-2 p-2 rounded-md">
+          <details open={!diffusionMessageRead}>
+            <summary
+                className="text-lg mb"
+                onClick={setDiffusionMessageAsRead}>👩‍⚖️ -- {diffusionMessageRead ? 'Rappel: diffusion interne uniquement' : 'À lire attentivement avant utilisation' } -- 👨‍⚖️</summary>
+            <div className="p-2">
+              <p>
+                Listing de support d&apos;upskilling pour les consultants Zenika.
+                Ces médias sont des productions internes, et ne doivent pas être
+                diffusées hors de Zenika sauf mention contraire explicite{" "}
+                <IconPublic />. Chaque média est daté et le contexte de présentation
+                est précisé ici.
+              </p>
+              <p>
+                Sont éligibles ici les vidéos avec contenu de formation, avancé ou
+                non, permettant aux consultants internes de découvrir ou
+                d&apos;approfondir un sujet. Les contenus commerciaux ne sont pas la
+                cible de cette librairie.
+              </p>
+              <p>
+                Pourquoi un Site ? Parce qu&apos;on est trop mignon, et que git
+                n&apos;est pas adapté à toutes les populations 🤔🧌 Si vous avez
+                mieux comme outils, n&apos;hésitez pas 😄
+              </p>
+              <p>
+                La documentation technique et la documentation sur l&apos;ajout d&apos;un talk est
+                disponible&nbsp;
+                <a
+                  href="https://docs.google.com/document/d/1FMLZWCNmCYxix0cRr0Ars0UnaZvoQQpRdRNnXGIzKOo/edit?usp=sharing"
+                  target="_blank"
+                  title="Lien vers la documentation de TheZaurus"
+                  rel="noopener"
+                >
+                  ici
+                </a>
+                . Vous pouvez également accéder directement au formulaire d&apos;ajout en cliquant sur&nbsp;
+                <a
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSfF99AC8fEKcXXF6z-gZMeCCtSGyUvQYxjO03Z1SkP2k3yNrg/viewform"
+                  target="_blank"
+                  title="Lien vers le formulaire d'ajout"
+                  rel="noopener"
+                >
+                  ce lien
+                </a>.
+              </p>
+            </div>
+          </details>
         </section>
+
         <Talks />
 
         <footer className="text-xs text-center py-2 border-t">
