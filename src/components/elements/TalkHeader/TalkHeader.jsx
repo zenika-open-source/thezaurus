eaffa8b63cbcc511c117c8a8ed1d5959ddcdfb9f
Will Munn <william.munn@zenika.com>
2023-09-22 10:34:31 +0200

    chore: Extract constants for video and talk strings

--- src/components/elements/TalkHeader/TalkHeader.jsx
+++ src/components/elements/TalkHeader/TalkHeader.jsx
@@ -3,16 +3,17 @@ import Resource from "../Ressource/Ressource";
 import { Open_Sans } from "next/font/google";
 import styles from "./TalkHeader.module.css";
 
+const videoIcon = "talk";
+const videoFormat = "video";
 const openSans = Open_Sans({ subsets: ["latin"] });
 
 export default function TalkHeader({ talk }) {
   const isPrivate = !talk.format.includes("public");
-  console.log(talk.format);
   return (
     <header className={`${styles.card__header} ${openSans.className}`}>
       <ul className="flex w-3/5 gap-8">
         {talk.format.map((fmt, i) => {
-          const format = fmt === "video" ? "talk" : fmt;
+          const format = fmt === videoFormat ? videoIcon : fmt;
           return (
             <li key={i}>
               <HeaderIcon icon={format} text={format} />
